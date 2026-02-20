951c04cd655f8d9060e8e561c1834e5665e265fb
Nathan Degorce <nathandegorce@yahoo.fr>
2026-02-20 02:20:12 +0100

    feat: charger la galerie depuis l’API

---
+++ FrontEnd/assets/script.js
@@ -0,0 +1,27 @@
+// Récupérer les travaux depuis l'API
+fetch("http://localhost:5678/api/works")
+  .then(response => response.json())
+  .then(works => {
+    // Récupérer l'élément HTML de la galerie
+    const gallery = document.querySelector(".gallery");
+    
+    // Pour chaque projet, créer un élément HTML
+    works.forEach(work => {
+      // Créer une figure
+      const figure = document.createElement("figure");
+      
+      // Créer l'image
+      const img = document.createElement("img");
+      img.src = work.imageUrl;
+      img.alt = work.title;
+      
+      // Créer le titre
+      const figcaption = document.createElement("figcaption");
+      figcaption.textContent = work.title;
+      
+      // Assembler le tout
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+    });
+  });
