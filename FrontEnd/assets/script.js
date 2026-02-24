e5748638c43e46e299ba30fa98eb428a4830f90b
Nathan Degorce <nathandegorce@yahoo.fr>
2026-02-24 15:42:23 +0100

    feat:mise en marche des filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,31 +1,28 @@
 // Récupérer les travaux depuis l'API
+let allWorks =[]
 fetch("http://localhost:5678/api/works")
   .then(response => response.json())
   .then(works => {
-    // Récupérer l'élément HTML de la galerie
-    const gallery = document.querySelector(".gallery");
-    
-    // Pour chaque projet, créer un élément HTML
-    works.forEach(work => {
-      // Créer une figure
-      const figure = document.createElement("figure");
-      
-      // Créer l'image
-      const img = document.createElement("img");
-      img.src = work.imageUrl;
-      img.alt = work.title;
-      
-      // Créer le titre
-      const figcaption = document.createElement("figcaption");
-      figcaption.textContent = work.title;
-      
-      // Assembler le tout
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
-    });
+    allWorks = works;
+    afficherGalerie(allWorks);
   });
 
+function afficherGalerie(listeTravaux) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = "";
+  
+  listeTravaux.forEach(work => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  });
+}
   fetch("http://localhost:5678/api/categories")
   .then(response => response.json())
   .then(categories => {
@@ -33,11 +30,21 @@ fetch("http://localhost:5678/api/works")
     
     const btnAll = document.createElement("button");
     btnAll.textContent = "Tous";
+    btnAll.addEventListener('click', function() {
+    afficherGalerie(allWorks);
+    });
     filtersContainer.appendChild(btnAll);
     
     categories.forEach(category => {
       const btn = document.createElement("button");
       btn.textContent = category.name;
+        btn.addEventListener('click', function() {
+    const filtres = allWorks.filter(work => work.categoryId === category.id);
+    afficherGalerie(filtres);
+  });
       filtersContainer.appendChild(btn);
     });
   });
+
+
+ 
